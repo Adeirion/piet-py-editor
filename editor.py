0315a5c602ca8ef1ec12450b0a9f905cb8ac8e72
Adeirion <antoine.adam.1789@gmail.com>
2017-01-29 14:48:21 +0100

    adding save and open images

--- editor.py
+++ editor.py
@@ -1,7 +1,13 @@
 import sys
+import os
+from PIL import Image, ImageDraw
 
-from interpreter import Program
-
+# from interpreter import Program
+try:
+	execfile("D:\Antoine\Programmation\piet-py-editor\interpreter.py")
+except Exception:
+	pass
+	
 #python 2
 if sys.version_info.major == 2:
 	from Tkinter import Tk, BOTH, Menu, Grid, Canvas, X, Y, LEFT, TOP, RAISED,  SUNKEN, VERTICAL, HORIZONTAL, RIGHT, BOTTOM, SCROLL, UNITS, Text, StringVar, Button, Label, Frame, Scrollbar
@@ -38,6 +44,48 @@ color_names = {'lred' : 'light coral',
 		'white':'white',
 		'black':'black'}
 
+color_rgbs = {'lred' : (240,128,128),
+		'red'  : (255,0,0),
+		'dred' : (139,0,0),
+		'lyellow': (238,221,130),
+		'yellow' : (255,215,0),
+		'dyellow': (218,165,32),
+		'lgreen': (152,251,152),
+		'green' : (0,238,0),
+		'dgreen': (0,100,0),
+		'lcyan': (175,238,238),
+		'cyan' : (0,255,255),
+		'dcyan': (0,134,139),
+		'lblue': (100,149,237),
+		'blue' : (0,0,255),
+		'dblue': (0,0,128),
+		'lmagenta': (255,105,180),
+		'magenta' : (255,0,255),
+		'dmagenta': (139,0,139),
+		'white':(255,255,255),
+		'black':(0,0,0)}
+
+rgb_colors = {(240,128,128):'lred',
+		(255,0,0) : 'red',
+		(139,0,0) : 'dred',
+		(238,221,130) : 'lyellow',
+		(255,215,0) : 'yellow',
+		(218,165,32) : 'dyellow',
+		(152,251,152) : 'lgreen',
+		(0,238,0) : 'green' ,
+		(0,100,0) : 'dgreen',
+		(175,238,238) : 'lcyan',
+		(0,255,255) : 'cyan',
+		(0,134,139) : 'dcyan',
+		(100,149,237) : 'lblue',
+		(0,0,255) : 'blue',
+		(0,0,128) : 'dblue',
+		(255,105,180) : 'lmagenta',
+		(255,0,255) : 'magenta',
+		(139,0,139) : 'dmagenta',
+		(255,255,255) : 'white',
+		(0,0,0) : 'black'}
+
 color_matrix = [['lred', 'red', 'dred'],
 			 ['lyellow', 'yellow', 'dyellow'],
 			 ['lgreen', 'green', 'dgreen'],
@@ -56,6 +104,9 @@ class PietEditorFrame(Frame):
 
 	def __init__(self, parent):
 		Frame.__init__(self, parent)   
+
+		self.frontcolor="white"
+		self.backcolor="black"
 
 		self.initUI()
 		
@@ -82,9 +133,10 @@ class PietEditorFrame(Frame):
 
 		# create a pulldown menu, and add it to the menu bar
 		filemenu = Menu(menubar, tearoff=0)
-		filemenu.add_command(label="Open...", command=self.hello)
-		filemenu.add_command(label="Save", command=self.hello)
-		filemenu.add_command(label="Save as...", command=self.hello)
+		filemenu.add_command(label="New...", command=self.onnew)
+		filemenu.add_command(label="Open...", command=self.canvasframe.open)
+		filemenu.add_command(label="Save", command=self.canvasframe.onsave)
+		filemenu.add_command(label="Save as...", command=self.canvasframe.onsaveas)
 		filemenu.add_separator()
 		filemenu.add_command(label="Run", command=self.onrun)
 		filemenu.add_separator()
@@ -106,8 +158,15 @@ class PietEditorFrame(Frame):
 	def onrun(self, event=None):
 		print "run"
 	
+	def onnew(self, event=None):
+		#check if saved
+		self.canvasframe.new_program(30,30)
+	
 	def getfrontcolor(self):
-		return self.colorselector.getfrontcolor()
+		return self.frontcolor
+
+	def getbackcolor(self):
+		return self.backcolor
 
 	def hello(self):
 		print("hello")
@@ -164,7 +223,7 @@ class ColorFrame(Frame):
 		self.frontcolorlabel.grid(row=15, column=1, rowspan=2, columnspan=5, sticky='NSEW', padx=10, pady=10)
 
 	def newfrontcolor(self, color):
-		self.frontcolor=color
+		self.parent.frontcolor=color
 		self.frontcolorlabel.config(background=color_names[color])
 		
 		for r in range(6):
@@ -176,13 +235,8 @@ class ColorFrame(Frame):
 					return
 
 	def newbackcolor(self, color):
-		self.backcolor=color
+		self.parent.backcolor=color
 		self.backcolorlabel.config(background=color_names[color])
-
-	def getfrontcolor(self):
-		return self.frontcolor
-	def getbackcolor(self):
-		return self.backcolor
 		
 class CanvasFrame(Frame):
 
@@ -192,7 +246,6 @@ class CanvasFrame(Frame):
 		self.parent=parent
 		
 		self.codel_size=20
-		self.program = Program(30,30)
 	
 		self.canvas = Canvas(self, relief=SUNKEN, borderwidth=1, background="grey", scrollregion=(0,0,500,500))
 		
@@ -206,27 +259,99 @@ class CanvasFrame(Frame):
 		self.canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
 		self.canvas.pack(side=LEFT, fill=BOTH, expand=1)
 
+		self.new_program(30,30)
+		
+	def new_program(self,height,width):
+		
+		self.program = Program(height,width)
+		self.openfile = ""
+		self.cwd=os.getcwd()
+		self.modified=True
+	
+		self.canvas.delete('all')
+		
 		for i in range(self.program.height):
-			#self.canvas.create_line(0, i*self.codel_size, self.image_height*self.codel_size, i*self.codel_size)
 			for j in range(self.program.width):
 				self.canvas.create_rectangle(j*self.codel_size,i*self.codel_size, (j+1)*self.codel_size, (i+1)*self.codel_size, fill='white')
 
 		self.canvas.tag_bind('all', '<Button-1>', self.onclick)
+		self.canvas.tag_bind('all', '<Button-3>', self.onclick)
+		
+	def onsave(self):
+		self.save(self.openfile)
+
+	def onsaveas(self):
+		self.save("")
+
+	def save(self, savefile=""):
+
+		if savefile=="":
+		
+			savefile = asksaveasfilename(defaultextension=".png", filetypes=[("Image Files", ".png")], initialdir=self.cwd, initialfile=self.openfile,parent=self, title="Save as")
+
+			if savefile=="":
+				return
+		
+		image = Image.new("RGB", (self.program.width*self.codel_size,self.program.height*self.codel_size), (255,255,255))
+		draw = ImageDraw.Draw(image)
+		for i in range(self.program.height):
+			for j in range(self.program.width):
+				draw.rectangle((j*self.codel_size,i*self.codel_size,(j+1)*self.codel_size,(i+1)*self.codel_size), fill = color_rgbs[self.program.get_codel(i,j)])
+		image.save(savefile)
+		
+		self.cwd = os.path.dirname(savefile)
+		self.openfile=savefile
+		self.modified=False
+		
+		
+	def open(self, openfile=""):
+
+		if openfile=="":
+
+			openfile = askopenfilename(defaultextension=".png", filetypes=[("Image files", ".png")], initialdir=self.cwd, initialfile="",parent=self, title="Open image")
+		
+			if openfile=="":
+				return
+
+			image = Image.open(openfile)
+			bbox= image.getbbox()
+			width = bbox[2]/self.codel_size
+			height = bbox[3]/self.codel_size
+			self.new_program(height, width)
+			for x in range(0, bbox[2], self.codel_size):
+				for y in range(0, bbox[3], self.codel_size):
+					color=rgb_colors[image.getpixel((x, y))]
+					self.canvas.itemconfig(self.canvas.find_closest(x,y), fill=color_names[color])
+					self.program.set_codel(y/self.codel_size, x/self.codel_size,color)
+			
+			self.cwd = os.path.dirname(openfile)
+			self.openfile = openfile
+			self.modified=False
+			
+	def asksave(self):
+		if self.modified == True:
+			if askyesno("Save", "Save file?"):
+				self.onsave()
+				return self.openfile
+			else:
+				return None
+		return self.openfile
 
 	def onclick(self, event):
-		
-		print event.x,event.y
 
 		x = self.canvas.canvasy(event.x)-3
 		y = self.canvas.canvasy(event.y)-3
 
-		print x,y
 		if x<0 or y<0:
 			return
 
-		frontcolor = self.parent.getfrontcolor()
+		if event.num==1:
+			color = self.parent.getfrontcolor()
+		else:
+			color = self.parent.getbackcolor()
 
-		self.canvas.itemconfig(self.canvas.find_closest(x,y), fill=color_names[frontcolor])
+		self.canvas.itemconfig(self.canvas.find_closest(x,y), fill=color_names[color])
+		self.modified=True
 
 		#!!!!! y in rows and x in columns
 		row = int(y)/self.codel_size
@@ -235,16 +360,15 @@ class CanvasFrame(Frame):
 		column = int(x)/self.codel_size
 		if column == self.program.width:
 			column=column-1
-		self.program.set_codel(row,column,frontcolor)
+		self.program.set_codel(row,column,color)
 
-		print row,column
 
 
 def main():
 
 	root = Tk()
 
-	root.geometry("1000x500+300+300")
+	root.geometry("1000x500+150+150")
 	app = PietEditorFrame(root)
 	root.mainloop()  
 
